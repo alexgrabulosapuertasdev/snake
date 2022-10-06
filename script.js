27804d375889974dcb2f80c1aebe37486f9c2a77
Àlex Grabulosa Puertas <alexgrabulosapuertas9@gmail.com>
2022-10-06 21:52:30 +0200

    Feat: Implment scoring system

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector('canvas');
 canvas.width = canvas.height = 400;
 const ctx = canvas.getContext('2d');
+const score = document.getElementById('score');
 
 const SIZE = 20;
 
+let apple;
 let direction;
 let snake;
 
@@ -28,6 +30,25 @@ function draw() {
         ctx.fillRect(snake[i].x, snake[i].y, SIZE, SIZE);
         ctx.strokeRect(snake[i].x, snake[i].y, SIZE, SIZE);
     }
+
+    ctx.fillStyle = '#FB1219';
+    ctx.fillRect(apple.x, apple.y, SIZE, SIZE);
+}
+
+function generateApple() {
+    const blocks = snake.map(block => ({ x: block.x, y: block.y }));
+    let positionValid;
+    let x;
+    let y;
+    
+    do {
+        x = Math.floor(Math.random() * canvas.width / SIZE) * SIZE;
+        y = Math.floor(Math.random() * canvas.height / SIZE) * SIZE;
+        
+        positionValid = !blocks.find(block => block.x === x && block.y === y);
+    } while(!positionValid);
+
+    apple = { x, y };
 }
 
 function move() {
@@ -53,8 +74,17 @@ function move() {
     const nextMove = DIRECTIONS_KEY_CODE[direction];
     if (!nextMove) return;
 
-    snake.pop();
     snake.unshift(nextMove);
+
+    const blocks = snake.map(block => ({ x: block.x, y: block.y }));
+    const touchingApple = blocks.find(block => block.x === apple.x && block.y === apple.y);
+    if (touchingApple) {
+        upScore();
+        generateApple();
+        return;
+    }
+    
+    snake.pop();
 
     const { height, width } = canvas;
     if (x >= width || x < 0 || y >= height || y < 0) {
@@ -64,6 +94,11 @@ function move() {
 
 function resetValues() {
     document.removeEventListener('keydown', selectDirection);
+    const { height, width } = canvas;
+    apple = {
+        x: width / 2,
+        y:  height / 2,
+    };
     direction = undefined;
     snake = [
         { x: 4 * SIZE, y: 4 * SIZE },
@@ -83,4 +118,8 @@ function start() {
     document.addEventListener('keydown', selectDirection);
     intervalDraw = setInterval(draw, 100);
     intervalMove = setInterval(move, 100);
+}
+
+function upScore() {
+    score.innerText = Number(score.innerText) + 1;
 }
